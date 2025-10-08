c3d031ffeb4d86501144667f79c68c5276d13f4a
BaconEater314 <nickbeaty314@gmail.com>
2025-10-08 10:58:35 -0600

    class work 10/8/2025

--- Source/Application/Main.cpp
+++ Source/Application/Main.cpp
@@ -1,3 +1,4 @@
+using namespace neu;
 
 int main(int argc, char* argv[]) {
     neu::file::SetCurrentDirectory("Assets");
@@ -12,6 +13,44 @@ int main(int argc, char* argv[]) {
 
     SDL_Event e;
     bool quit = false;
+
+    //std::vector<vec3> points = { {0.5,0.5,0}, {0.5,-0.5,0},{-0.5,0.5,0} };
+    //std::vector<vec3> colors = { {1,1,0},{0,1,1},{1,0,1}};
+    std::vector<vec3> points{ { -0.5f, -0.5f, 0 }, { 0, 0.5f, 0 }, { 0.5f, -0.5f, 0 } }; 
+    std::vector<vec3> colors{ { 1, 0, 0 }, { 0, 1, 0 }, { 0, 0, 1 } };
+
+    GLuint vbo;
+    glGenBuffers(1, &vbo);
+
+    //vertext buffer (position)
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * points.size(), points.data(), GL_STATIC_DRAW);
+
+    GLuint vao;
+    glGenVertexArrays(1, &vao);
+
+    glEnableVertexAttribArray(0);
+    glBindBuffer(GL_ARRAY_BUFFER, vbo);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);
+
+    //vertex shader
+    std::string vs_source;
+    file::ReadTextFile("shaders/basic.vert", vs_source);
+    const char* vs_cstr = vs_source.c_str();
+
+    GLuint vs;
+    vs = glCreateShader(GL_VERTEX_SHADER);
+
+    glShaderSource(vs, 1, &vs_cstr, NULL);
+    glCompileShader(vs);
+
+    //fragment shader
+
+
+    //float angle = GetEngine().GetTime().GetTime()*90.0f;
+    //float scale = math::Remap(-1.0f, 1.0f, 0.3f, 1.5f, math::sin(GetEngine().GetTime().GetTime()));
+
+    //vec2 mouse = GetEngine().G
 
     // MAIN LOOP
     while (!quit) {
@@ -31,16 +70,30 @@ int main(int argc, char* argv[]) {
         neu::GetEngine().GetRenderer().SetColor(color.r, color.g, color.b);
         neu::GetEngine().GetRenderer().Clear();
 
-        glBegin(GL_TRIANGLES);
+        glLoadIdentity();
+        glPushMatrix();
+        //glTranslate(position.x, position.y);
+
+        /*glBegin(GL_TRIANGLES);
 
         glColor3f(1,0,0);
         glVertex3f(0.5,0.5,0);
         glColor3f(0,1,0);
         glVertex3f(0.5,-0.5,0);
         glColor3f(0,0,1);
-        glVertex3f(-0.5,0.5,0);
+        glVertex3f(-0.5,0.5,0);*/
+
+        glRotatef(angle,0,0,1);
+        //glScalef(scale, scale, 0);
+
+        glBegin(GL_TRIANGLES);
+        for (int i = 0; i < 3; i++) {
+            glColor3f(colors[i].r, colors[i].g, colors[i].b);
+            glVertex3f(points[i].x, points[i].y, points[i].z);
+        }
 
         glEnd();
+
 
         neu::GetEngine().GetRenderer().Present();
     }
